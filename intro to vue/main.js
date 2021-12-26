44c396e273345665a0d0cf0005fdb03551fa71a0
David Marshall <dlmarshall3@protonmail.com>
2021-12-25 23:59:08 -0600

    finished attribute binding, conditional rendering, list rendering, and event handling

--- intro to vue/main.js
+++ intro to vue/main.js
@@ -1,6 +1,34 @@
-var product = new Vue({
+var app = new Vue({
     el: '#app',
     data: {
-        product: 'Socks'
+        product: 'Socks',
+        image: 'img/greensocks.jpeg',
+        link: 'http://www.vuemastery.com',
+        inventory: 8,
+        details: ["80% cotton",'20% polyester','Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'Green',
+                variantImage: 'img/greensocks.jpeg'
+            },
+            {
+                variantId: 2235,
+                variantColor: "Blue",
+                variantImage: 'img/bluesocks.jpeg'
+            }
+        ],
+        cart: 0,
+    },
+    methods: {
+        addToCart: function(){
+            this.cart ++
+        },
+        updateProduct: function(variantImage){
+            this.image = variantImage
+        },
+        removeFromCart: function(){
+            this.cart --
+        }
     }
 })
